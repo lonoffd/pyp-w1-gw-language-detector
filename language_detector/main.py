8da8a980e14e56f6b3755d72607f0218aed27a08
David Lonoff <david.lonoff+pyp@gmail.com>
2017-04-28 01:12:23 +0000

    Refactored code!

--- language_detector/main.py
+++ language_detector/main.py
@@ -5,16 +5,15 @@
 def detect_language(text, languages):
     """Returns the detected language of given text."""
     word_list = text.split()
-    word_set = set(word_list)
-    word_counts = {word: word_list.count(word) for word in word_set}
 
-    spanish_set = set(languages[0]['common_words'])
-    german_set = set(languages[1]['common_words'])
-    english_set = set(languages[2]['common_words'])
-    is_spanish = list(filter(lambda x: x in spanish_set, word_list))
-    is_german = list(filter(lambda x: x in german_set, word_list))
-    is_english = list(filter(lambda x: x in english_set, word_list))
-    language_lists = [(len(is_spanish), "Spanish"), (len(is_english), "English"), (len(is_german), "German")]
+    language_lists = []
+    
+    for lang in languages:
+        lang_name = lang['name']
+        lang_set = set(lang['common_words'])
+        lang_word_list = list(filter(lambda x: x in lang_set, word_list))
+        language_lists.append((len(lang_word_list), lang_name))
+    
     language_lists.sort(reverse=True)
     return language_lists[0][1]
 
